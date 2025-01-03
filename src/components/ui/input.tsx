be2fe2ce83d468595efb757ad43a25ae62e2e9fb
huuhung7301 <48995642+huuhung7301@users.noreply.github.com>
2025-01-03 23:37:05 +1100

    update

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,16 +1,12 @@
-import * as React from "react"
+import * as React from "react";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export function Input({
+  className = "",
+  ...props
+}: React.InputHTMLAttributes<HTMLInputElement>) {
+  const baseClasses = "flex w-full bg-transparent";
 
-export function Input({ className = "", ...props }: InputProps) {
-  const baseClasses = "flex w-full bg-transparent"
-  
   return (
-    <input
-      className={`${baseClasses} ${className}`}
-      {...props}
-    />
-  )
+    <input className={`${baseClasses} ${className}`} {...props} />
+  );
 }
-
